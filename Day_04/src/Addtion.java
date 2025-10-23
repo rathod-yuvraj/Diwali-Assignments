3a82c8871e731149ba6288f0643feb602414e57f
Yuvraj Rathod <yuvrajrathod8390@gmail.com>
2025-10-23 13:19:37 +0530

    :Day_04

---
+++ Day_04/src/Addtion.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Addtion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int a,b;
+		System.out.println("Enter number: ");
+		
+		a=sc.nextInt();
+		System.out.println("Enter number: ");
+		b=sc.nextInt();
+		System.out.println("Addtions: "+a+b);
+		
+		sc.close();
+
+	}
+
+}
